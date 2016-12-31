aa6f90276806dcd6b7d3aebee005de7a0e2bb17d
Kelvin Ly <kelvin.ly1618@gmail.com>
2016-12-31 10:00:16 -0500

    Add wall collisions and stuff

--- crabs.js
+++ crabs.js
@@ -53,67 +53,167 @@ var spring = function(ida, idb, l, k) {
   }
 }
 
+var time = function(t, f) {
+  var start = performance.now()
+  f()
+  var end = performance.now()
+  t = end - start
+}
+
 function Physics() {
+  this.DEFAULT_MASS = 1.0
+
   this.masses = {}
   this.springs = {}
 
-  this.f = 0.05
-  this.g = new Vec(0.0, -10.0)
+  this.f = 0.0005
+  this.g = new Vec(0.0, -2.0)
+  this.slip = 0.2
+  this.bounce = 0.998
+  this.dt = 1
+
+  this.height = 520
+  this.width = 1024
+
+  this.runtime = 0.0
 
   this.run = function(crabs) {
     var me = this
-    crabs.ids.forEach(function(id) {
-      if (me.springs[id]) {
-        var spring = me.springs[id]
-        var a = me.masses[spring.ida]
-        var b = me.masses[spring.idb]
-        var r = Vec.sub(a.pos, b.pos)
-        var f = Vec.scale(spring.k * (Vec.mag(r) - spring.l), Vec.norm(r))
-      }
-      if (me.masses[id]) {
-        var mass = me.masses[id]
-        mass.force = Vec.add(Vec.scale(-me.f, mass.vel))
-        mass.force = Vec.add(Vec.scale(mass.m, me.g))
-      }
+    time(me.runtime, function() {
+      crabs.ids.forEach(function(id) {
+        if (me.springs[id]) {
+          var spring = me.springs[id]
+          var a = me.masses[spring.ida]
+          var b = me.masses[spring.idb]
+          var r = Vec.sub(a.pos, b.pos)
+          var f = Vec.scale(spring.k * (Vec.mag(r) - spring.l), Vec.norm(r))
+        }
+        if (me.masses[id]) {
+          var mass = me.masses[id]
+          mass.forc = Vec.add(mass.forc, Vec.scale(-me.f, mass.vel))
+          mass.forc = Vec.add(mass.forc, Vec.scale(mass.m, me.g))
+        }
+      })
+      crabs.ids.forEach(function(id) {
+        if (me.masses[id]) {
+          var mass = me.masses[id]
+          mass.vel = Vec.add(mass.vel, Vec.scale(1.0/mass.m, mass.forc))
+          mass.pos = Vec.add(mass.pos, Vec.scale(me.dt, mass.vel))
+          mass.forc = Vec.zero()
+        }
+      })
+      crabs.ids.forEach(function(id) {
+        if (me.masses[id]) {
+          var mass = me.masses[id]
+          var collisionTime = null
+          if (mass.pos.x < 0.0) { 
+            collisionTime = -mass.pos.x / mass.vel.x
+          }
+          if (mass.pos.x > me.width) {
+            collisionTime = -(mass.pos.x - me.width) / mass.vel.x
+          }
+          if (collisionTime != null) {
+            var collisionPos = Vec.add(mass.pos, Vec.scale(collisionTime, mass.vel))
+            mass.vel = new Vec(-me.bounce*mass.vel.x,
+                              (1.0 - me.slip*Math.abs(mass.vel.x/Vec.mag(mass.vel)))*mass.vel.y)
+            mass.pos = Vec.add(collisionPos, Vec.scale(-collisionTime, mass.vel))
+          }
+
+          collisionTime = null
+          if (mass.pos.y < 0.0) {
+            collisionTime = -mass.pos.y / mass.vel.y
+          }
+          if (mass.pos.y > me.height) {
+            collisionTime = -(mass.pos.y - me.height) / mass.vel.y
+          }
+          if (collisionTime != null) {
+            var collisionPos = Vec.add(mass.pos, Vec.scale(collisionTime, mass.vel))
+            mass.vel = new Vec((1.0 - me.slip*Math.abs(mass.vel.y/Vec.mag(mass.vel)))*mass.vel.x,
+                              -me.bounce*mass.vel.y)
+            mass.pos = Vec.add(collisionPos, Vec.scale(-collisionTime, mass.vel))
+          }
+        }
+      })
     })
-    crabs.ids.forEach(function(id) {
-      if (me.masses[id]) {
-        var mass = me.masses[id]
-        mass.vel = Vec.add(mass.vel, Vec.scale(1.0/mass.m, mass.force))
-        mass.pos = Vec.add(mass.pos, Vec.scale(me.dt, mass.vel))
-        mass.force = Vec.zero()
-      }
-    })
-    // TODO: collision check walls
+  }
+
+  this.resize = function(crabs) {
+    // do nothing
+    return
+  }
+
+  this.addMass = function(id, pos, vel, m) {
+    this.masses[id] = mass(pos.x, pos.y, vel.x, vel.y, m)
   }
 }
 
 function Renderer() {
+  this.runtime = 0.0
+  this.MASS_RADIUS = 3.0
+  this.MASS_SELECT_RADIUS = 7.0
+
+  this.transform = function(crabs, vec) {
+    return new Vec(vec.x, crabs.height - vec.y)
+  }
+
   this.run = function(crabs) {
     var canvas = crabs.canvas.getContext('2d')
-    var physics = crabs.physics
-    crabs.id.forEach(function(id) {
-      if (physics.masses[id]) {
-        var mass = physics.masses[id]
-        if (crabs.ui.selected == id) {
-        }
-      }
-      if (physics.springs[id]) {
-        var spring = physics.springs[id]
-        var a = physics.masses[spring.ida],
-            b = physics.masses[spring.idb]
-        if (physics.muscles[id]) {
-          // TODO: draw muscle
-        } else {
-          // TODO: draw spring
-        }
+    var ui = crabs.uiCanvas.getContext('2d')
+    var me = this
+    time(me.runtime, function() {
+      canvas.clearRect(0, 0, crabs.width, crabs.height)
+      ui.clearRect(0, 0, crabs.width, crabs.height)
 
-        if (crabs.ui.selected == id) {
-          // TODO: draw selection symbol
+      var physics = crabs.physics
+      crabs.ids.forEach(function(id) {
+        if (physics.masses[id]) {
+          var mass = physics.masses[id]
+          // TODO: draw mass
+          var screenPos = me.transform(crabs, mass.pos)
+          canvas.fillStyle = '#222222'
+          canvas.beginPath()
+          canvas.arc(screenPos.x, screenPos.y, me.MASS_RADIUS, 0, Math.PI*2)
+          canvas.fill()
+          // log(log.DEBUG, 'mass ' + id + ' at ' + screenPos.x + ', ' + screenPos.y)
+
+          if (crabs.ui.selected == id) {
+            // TOOD: draw selection symbol
+            canvas.strokeStyle = '#000000'
+            canvas.beginPath()
+            canvas.arc(screenPos.x, screenPos.y, 0, Math.PI*2, me.MASS_SELECT_RADIUS)
+            canvas.stroke()
+          }
         }
-      }
+        if (physics.springs[id]) {
+          var spring = physics.springs[id]
+          var a = physics.masses[spring.ida],
+              b = physics.masses[spring.idb]
+          if (physics.muscles[id]) {
+            // TODO: draw muscle
+          } else {
+            // TODO: draw spring
+          }
+
+          if (crabs.ui.selected == id) {
+            // TODO: draw selection symbol
+          }
+        }
+      })
+
+      // ui.clearRect(0, 0, 40, 200)
+      // TODO: draw UI
     })
-    // TODO: draw UI
+
+    ui.font = '10px sans-serif'
+    ui.fillStyle = '#808080'
+    ui.fillText(crabs.physics.runtime + '/' + crabs.renderer.runtime,
+        0, 10)
+    // TODO: draw timings/frame rate
+    // log(log.DEBUG, 'timings: ' + crabs.physics.runtime + ' ms, ' +
+    //                              crabs.renderer.runtime + ' ms')
+  }
+
+  this.resize = function(crabs) {
   }
 }
 
@@ -136,12 +236,15 @@ function TestUI() {
   }
 }
 
-function Crabs(canvas) {
+function Crabs(canvas, ui) {
   this.canvas = canvas
+  this.uiCanvas = ui
   this.ids = []
+  this.lastId = -1
   this.physics = new Physics()
   this.renderer = new Renderer()
   this.ui = new UI()
+  this.components = [this.physics, this.renderer]
   this.hooks = [this.ui, new TestUI()]
   this.paused = false
   this.width = canvas.width
@@ -156,16 +259,18 @@ function Crabs(canvas) {
         return typeof(l[name]) != 'undefined'
       })
       log(log.INFO, 'adding hooks for ' + name + ': ' + listeners)
-      canvas.addEventListener(name, function(e) {
+      ui.addEventListener(name, function(e) {
         listeners.forEach(function(l) {
           l[name](me, e)
         })
       })
     })
+
+    this.resize()
   }
   this.go = function() {
     if (!this.paused) {
-      [this.physics, this.renderer].forEach(function(c) {
+      this.components.forEach(function(c) {
         c.run(crabs)
       })
 
@@ -177,6 +282,35 @@ function Crabs(canvas) {
     // log(log.INFO, 'frame')
   }
   this.resize = function() {
-    log(log.ERR, 'resize unimplemented!')
+    var w = window.innerWidth, h = window.innerHeight
+    log(log.DEBUG, 'resize ' + w + ', ' + h)
+
+    this.width = w
+    this.height = h
+
+    canvas.width = w
+    canvas.height = h
+    ui.width = w
+    ui.height = h
+
+    this.components.forEach(function(c) {
+      c.resize(this)
+    })
+  }
+
+  this.addMass = function(pos, vel) {
+    var newIdNum = this.lastId + 1
+    var newId = newIdNum.toString()
+    this.ids.push(newId)
+    this.physics.addMass(newId, pos, vel, this.physics.DEFAULT_MASS)
+    this.lastId = newIdNum
+  }
+
+  this.addSpring = function(m0, m1, r, k) {
+    // TODO
+  }
+
+  this.addMuscle = function(m0, m1, r, k, l, p) {
+    // TODO
   }
 }
