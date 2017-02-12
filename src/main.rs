6f20e80adb0a1fcba74774ae65322817e17c8c2c
Kelvin Ly <kelvin.ly1618@gmail.com>
2017-02-12 15:20:04 -0500

    Hackish build working!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate glium;
 
+
 use std::cell::RefCell;
 use std::ptr::null_mut;
 use std::os::raw::{c_int, c_void};
@@ -102,4 +103,51 @@ pub fn set_main_loop_callback<F>(callback : F) where F : FnMut() {
             (*closure)();
         });
     }
+}
+
+
+pub mod backtrace_hack {
+
+extern crate libc;
+
+use self::libc::uintptr_t;
+use std::os::raw::{c_void, c_char, c_int};
+
+pub type backtrace_syminfo_callback =
+    extern fn(data: *mut c_void,
+              pc: uintptr_t,
+              symname: *const c_char,
+              symval: uintptr_t,
+              symsize: uintptr_t);
+pub type backtrace_full_callback =
+    extern fn(data: *mut c_void,
+              pc: uintptr_t,
+              filename: *const c_char,
+              lineno: c_int,
+              function: *const c_char) -> c_int;
+pub type backtrace_error_callback =
+    extern fn(data: *mut c_void,
+              msg: *const c_char,
+              errnum: c_int);
+pub enum backtrace_state {}
+
+#[no_mangle]
+pub extern "C" fn __rbt_backtrace_create_state(_filename: *const c_char,
+                                  _threaded: c_int,
+                                  _error: backtrace_error_callback,
+                                  _data: *mut c_void) -> *mut backtrace_state { 0 as *mut _ }
+
+#[no_mangle]
+pub extern "C" fn __rbt_backtrace_syminfo(_state: *mut backtrace_state,
+                             _addr: uintptr_t,
+                             _cb: backtrace_syminfo_callback,
+                             _error: backtrace_error_callback,
+                             _data: *mut c_void) -> c_int { 0 }
+
+#[no_mangle]
+pub extern "C" fn __rbt_backtrace_pcinfo(_state: *mut backtrace_state,
+                            _addr: uintptr_t,
+                            _cb: backtrace_full_callback,
+                            _error: backtrace_error_callback,
+                            _data: *mut c_void) -> c_int { 0 }
 }
